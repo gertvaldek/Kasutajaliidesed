7ea853a97a4dbcaf2c5f717c106a68211a967bc0
Gert <gertvaldek@hotmail.com>
2015-05-05 21:48:05 +0300

    SQL ühendus loodud

--- system/connection.php
+++ system/connection.php
@@ -3,7 +3,7 @@
  *
  *  !! COMMENTED OUT UNTIL REAL SQL USER PASS INSERTED !!
  *
- *
+ */
 session_start();
 $active = $_SESSION['active'];
 
@@ -11,7 +11,7 @@ $con = mysql_connect("d8487.mysql.zone.ee","d8487sa83256","83fGuK");
 if (!$con) {
     die('Could not connect: ' . mysql_error());
 }
-mysql_select_db("d8487sd83956", $con);
-*/
+mysql_select_db("d8487sd95841", $con);
+
 
 ?>
